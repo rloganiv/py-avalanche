31a3e38105e9420471508e63444ca1fdb127c4fc
Robert Lockwood Logan IV <rloganiv@gmail.com>
2016-06-22 12:31:19 -0700

    Event generation as a Poisson process implemented.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from util.bank import Bank
+from util.event import Generator
 import numpy as np
 
 # Globals to be made args
@@ -22,14 +23,18 @@ if __name__ == '__main__':
         bank.add_neighbor(bank_list[(i + 1) % n_banks])
 
     # Event generation
-    events = ['nft_deposit', 'nft_withdrawal', 'nft_loan_req',
-              'nft_loan_repay']
-    p_events = [deposit_rate, withdrawal_rate, loan_rate, repay_rate]
+    event_dict = {
+        'nft_deposit': deposit_rate,
+        'nft_withdrawal': withdrawal_rate,
+        'nft_loan_req': loan_rate,
+        'nft_loan_repay': repay_rate
+    }
+    event_gen = Generator(event_dict)
 
     # Main loop
     while(n_banks > 0 and iterations < max_iter):
         # Generate event
-        event = np.random.choice(events, p = p_events)
+        event = event_gen.draw_event()[0]
         bank = np.random.choice(bank_list)
         if event == 'nft_deposit':
             bank.nft_deposit()
@@ -45,7 +50,7 @@ if __name__ == '__main__':
         if bank.dead:
             bank_list.remove(bank)
             n_banks += -1
-            #TODO: Add avalanche tracker properly define bank reaction logic
+            #TODO: Add avalanche tracker
             for neighbor in bank.interbank_balances:
                 neighbor.check_reserves()
                 neighbor.check_solvency()
--- util/event.py
+++ util/event.py
@@ -1,6 +1,23 @@
 import numpy as np
 
 class Generator(object):
+    """An event generator"""
     def __init__(self, event_dict):
-        pass
+        self.event_names = event_dict.keys()
+        self.event_rates = event_dict.values()
+        self.times = np.zeros(len(event_dict))
+        for i, event_rate in enumerate(self.event_rates):
+            self.times[i] = np.random.exponential(event_rate)
 
+    def draw_event(self):
+        ind = self.times.argmin()
+        elapsed = self.times[ind]
+        self.times = self.times - self.times[ind]
+        self.times[ind] = np.random.exponential(self.event_rates[ind])
+        return self.event_names[ind], elapsed
+
+if __name__ == "__main__":
+    event_dict = {'E1': 12., 'E2': .03}
+    gen = Generator(event_dict)
+    for i in xrange(1000):
+        print gen.draw_event()
